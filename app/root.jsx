116988c2a220b86ccfa5aa5439048a78a568ef26
wadoodalam <wadoodalam@outlook.com>
2024-10-22 20:13:44 -0700

    Custom App Shopify

---
+++ app/root.jsx
@@ -0,0 +1,32 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html>
+      {/* <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="preconnect" href="https://cdn.shopify.com/" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
+        />
+        <Meta />
+        <Links />
+      </head> */}
+      <body>
+        <p>Add the search/text area here</p>
+        {/* <h2>Testing</h2> */}
+        {/* <Outlet />
+        <ScrollRestoration />
+        <Scripts /> */}
+      </body>
+    </html>
+  );
+}
